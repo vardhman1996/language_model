3bff5406ed50ac4054489769dd817a918a39bac5
Vardhman Mehta <mehtav@cs.washington.edu>
2018-05-22 18:50:15 -0700

    changes to data layer to generate smaller timestep batches

--- data_reader.py
+++ data_reader.py
@@ -1,6 +1,8 @@
 import os
 import io
 import numpy as np
+import pickle as pkl
+import tensorflow as tf
 
 DATA_FOLDER = "data/"
 NUM_SENTENCES = 7560180 # total number of sentences collected
@@ -8,6 +10,10 @@ NUM_SENTENCES = 7560180 # total number of sentences collected
 MAX_TIME_STEPS = 98
 STOP_CHAR = '\u0003' # U+0003 \x03
 START_CHAR = '\u0002' # U+0002 \x02
+CHAR_TO_NUM_DICT = 'char_to_num.pkl'
+
+MAX_LENGTH = 25
+RANDOM_WINDOW = 20
 
 class DataReader:
     def __init__(self, filename, batch_size=128):
@@ -15,6 +21,7 @@ class DataReader:
         self.file = io.open(self.data_filename, 'r', encoding='utf-8')
         self.batch_size = batch_size
         self.stop_char_bits = self.char_to_bit(STOP_CHAR)
+        self.char_to_num = pkl.load(open(os.path.join(DATA_FOLDER, CHAR_TO_NUM_DICT), 'rb'))
 
     def read_data(self):
         data_list = []
@@ -33,16 +40,34 @@ class DataReader:
                 return data_list
 
     def format_data(self, char_list):
-        batch_list = np.zeros((self.batch_size, MAX_TIME_STEPS + 2, 32))
+        batch_list_x = np.zeros((self.batch_size, MAX_LENGTH, 32))
+        batch_list_y = np.zeros((self.batch_size,  1))
         for i, char_line in enumerate(char_list):
             char_line = [START_CHAR] + char_line + [STOP_CHAR]
-            batch_instance = np.zeros((MAX_TIME_STEPS + 2, 32))
-            for j, c in enumerate(char_line):
-                batch_instance[j] = self.char_to_bit(c)
+            batch_instance_x = np.zeros((MAX_LENGTH, 32))
+            batch_instance_y = None
 
-            batch_instance[len(char_line):] = self.stop_char_bits
-            batch_list[i] = batch_instance
-        return batch_list
+            slice_index = np.random.randint(0, RANDOM_WINDOW)
+            sliced_char_line = char_line[slice_index:slice_index+MAX_LENGTH]
+
+            if len(sliced_char_line) < MAX_LENGTH:
+                batch_instance_y = self.char_to_num[STOP_CHAR]
+            else:
+                if len(char_line) <= slice_index + MAX_LENGTH:
+                    batch_instance_y = self.char_to_num[STOP_CHAR]
+                else:
+                    batch_instance_y = self.char_to_num[char_line[slice_index + MAX_LENGTH]]
+
+            for j, c in enumerate(sliced_char_line):
+                batch_instance_x[j] = self.char_to_bit(c)
+
+            batch_instance_x[len(sliced_char_line):] = self.stop_char_bits
+
+            assert(batch_instance_y != None)
+            batch_list_x[i] = batch_instance_x
+            batch_list_y[i] = batch_instance_y
+
+        return batch_list_x, batch_list_y
 
 
     def char_to_bit(self, c):
@@ -56,11 +81,14 @@ class DataReader:
                 self.file.seek(0)
                 batch_char_list = self.read_data()
 
-            batch_char_bits = self.format_data(batch_char_list)
-            yield batch_char_bits
+            batch_x, batch_y = self.format_data(batch_char_list)
+
+            batch_y = tf.keras.utils.to_categorical(batch_y, num_classes=len(self.char_to_num))
+            yield batch_x, batch_y
+
 
 
 # EXAMPLE USAGE
 # dr = DataReader('final_sentences.csv', batch_size=128)
-# for i, batch in enumerate(dr.get_data(num_batches=100000)):
-#     print(i, batch.shape)
+# for i, (batch_x, batch_y)  in enumerate(dr.get_data(num_batches=100000)):
+#     print(i, batch_x.shape, batch_y.shape)
--- generate_mapping.py
+++ generate_mapping.py
@@ -10,6 +10,7 @@ DATA = "data/sentences.csv"
 DATA_TRAIN = "data/wili-2018/x_train.txt"
 DATA_TEST = "data/wili-2018/x_test.txt"
 DATA_PATH = 'data'
+STOP_CHAR = '\u0003' # U+0003 \x03
 
 
 def read_data_taboeta(datafile):
@@ -20,17 +21,31 @@ def read_data_taboeta(datafile):
 def trim_taboeta_sentences(data_df):
     languages = data_df['lang'].unique()
     char_set = set()
+    sum = 0.0
+    sent = 0.0
+
+    num_less_than = 0
+
     for i, lang in enumerate(languages):
         sentence_df = data_df.loc[data_df['lang'] == lang]
         data = sentence_df['sent'].values
         random.shuffle(data)
 
         for line in data:
+            sum += len(line)
+            sent += 1
+
+            if len(line) < 30:
+                num_less_than += 1
+
             char_set.update(c for c in line)
 
         if (i + 1) % 10 == 0:
             print("Completed: {0} out of {1}".format(i + 1, len(languages)))
     print("Num unique chars in Taboeta: {}".format(len(char_set)))
+
+    print("AVG = ", sum / sent)
+    print("NUM less = ", num_less_than)
     return char_set
 
 def read_data_wili(datafile):
@@ -68,10 +83,11 @@ def main():
     universal_char_set = universal_char_set | read_data_wili(DATA_TRAIN)
     universal_char_set = universal_char_set | read_data_wili(DATA_TEST)
 
+    universal_char_set.add(STOP_CHAR)
+
     print("Num unique chars: {}".format(len(universal_char_set)))
 
     char_to_num_map, num_to_char_map = make_mapping(universal_char_set)
-
     save_map(char_to_num_map, os.path.join(DATA_PATH, 'char_to_num.pkl'))
     save_map(num_to_char_map, os.path.join(DATA_PATH, 'num_to_char.pkl'))
 
