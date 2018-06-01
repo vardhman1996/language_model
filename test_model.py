3d6d573137f9de4790ed4646e76de2bd87838121
Vardhman Mehta <mehtav@cs.washington.edu>
2018-06-01 01:28:34 -0700

    test model changes

--- test_model.py
+++ test_model.py
@@ -2,15 +2,18 @@ import os
 import io
 
 def main():
-    test_file = 'tests/test4.txt'
+    test_file = 'data/utf8.txt'
     file = io.open(test_file, 'r', encoding='utf-8')
     data = file.read().strip()
 
     test_data = ""
     for char in data:
-        test_data += 'o' + char
+        test_data += 'q' + char
 
-    print(u"" + test_data)
+    test_data += 'x'
+
+    with io.open(test_file, 'w', encoding='utf-8') as out:
+        out.write(test_data)
 
 
 if __name__ == "__main__":
